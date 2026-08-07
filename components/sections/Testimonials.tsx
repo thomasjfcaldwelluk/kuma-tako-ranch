1cea4279d8df1636fcfbe95b7ca5f19b651a9a73
thomasjfcaldwell <thomasjfcaldwell@gmail.com>
2026-08-07 14:55:46 +0100

    half way deployment

--- components/sections/Testimonials.tsx
+++ components/sections/Testimonials.tsx
@@ -6,7 +6,7 @@ import Grid from "../layout/Grid";
 export default function Testimonials() {
   return (
     <Section>
-      <Grid cols={3} spacing="md">
+      <Grid cols={3} gap="lg">
         {testimonials.map((testimonial, index) => (
           <div key={index}>
             <p>{testimonial.quote}</p>
